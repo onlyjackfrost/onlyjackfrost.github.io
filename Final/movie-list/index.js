544b657521dde66611738ad7f0ed113496d83ad6
onlyjackfrost <onlyjackfrost@gmail.com>
2019-12-21 11:22:04 +0800

    movie done

--- Final/movie-list/index.js
+++ Final/movie-list/index.js
@@ -1,6 +1,9 @@
 
-  const apiBaseUrl = `https://movie-list.alphacamp.io/api/v1/`
-  const imgBaseUrl = `https://movie-list.alphacamp.io/posters/`
+const apiBaseUrl = `https://movie-list.alphacamp.io/api/v1/`
+const imgBaseUrl = `https://movie-list.alphacamp.io/posters/`
+const navElement = document.querySelector('.nav')
+const dataPanel = document.querySelector('#data-panel')
+
 const model = {
   categories : [
       "Action",
@@ -34,20 +37,54 @@ const controller = {
     const categoryElements = document.querySelectorAll('.nav-link')
     categoryElements.forEach((element)=>{
       element.addEventListener('click', (event) => {
-        view.navOnClickChange(element)
+        controller.navOnClickChange(element)
       })
     })
-
+    controller.requestAndSetMovies()
   },
 
-  requestMovies: ()=>{
-    const url = apiBaseUrl + 'index'
-    const res = axios
+  requestAndSetMovies: ()=>{
+    const url = apiBaseUrl + 'movies'
+    axios.get(url)
+      .then((res)=>{
+        console.log(res.data.results[0])
+        model.movies = res.data.results
+      })
+      .catch((err)=>{
+        console.log(err)
+      }).then(()=>{
+        view.showMovies(model.movies)
+      })
+  },
+
+  navOnClickChange: (element)=>{
+    controller.deactivateAllNav()
+    controller.setNavActive(element)
+    const tag = element.innerHTML
+    const movies = controller.filterMovie(tag)
+    console.log(movies)
+    view.showMovies(movies)
+  },
+
+  setNavActive:(element)=>{
+    element.classList.add('active')
+  },
+
+  deactivateAllNav:()=>{
+    const activeNav = document.querySelectorAll('.active')
+    activeNav.forEach((nav)=>{
+      nav.classList.remove('active')
+    })
+  },
+  filterMovie : (tag)=>{
+    return model.movies.filter((movie) => {
+      return util.convertIdx2TagName(movie.genres).some((ele)=>ele===tag)
+    })
   }
+
 }
 
-const navElement = document.querySelector('.nav')
-const dataPanel = document.querySelector('#data-panel')
+
 const view = {
   showCategories : (categories) => {
     const innerHtml = categories.reduce((html,category, idx)=>{
@@ -74,23 +111,31 @@ const view = {
   },
 
   displayMovieCard:(movie)=>{
-
+    const tags = util.convertIdx2TagName(movie.genres)
+    return `
+    <div class="col-sm-3">
+      <div class="card mb-2" data-index="${movie.id}">
+          <img class="card-img-top " src="${imgBaseUrl+movie.image}" alt="Card image cap">
+          <div class="card-body movie-item-body">
+              <h6 class="card-title">${movie.title}</h6>
+          </div>
+            <!-- tag list -->
+          <div class="card-footer d-flex flex-row flex-wrap">
+                ${tags.reduce((html, tag)=>{
+                  return html + `<div class="tag">${tag}</div>`
+                },'')}
+          </div>
+      </div>
+    </div>
+    `
   },
+}
 
-  navOnClickChange: (element)=>{
-    view.deactivateAllNav()
-    view.setNavActive(element)
-  },
-
-  setNavActive:(element)=>{
-    element.classList.add('active')
-  },
-
-  deactivateAllNav:()=>{
-    const activeNav = document.querySelectorAll('.active')
-    activeNav.forEach((nav)=>{
-      nav.classList.remove('active')
-    })
+const util={
+  convertIdx2TagName : (idxs)=>{
+    if(!Array.isArray(idxs)) return []
+    return idxs.map((idx)=>model.categories[idx-1])
   }
 }
+
 controller.init()
