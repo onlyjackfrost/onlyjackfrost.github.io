39dc605ab9cec7a44d68459eb78b8ac10a188a3b
onlyjackfrost <onlyjackfrost@gmail.com>
2019-12-21 13:54:05 +0800

    mvp done

---
+++ Final/hexConverter/index.js
@@ -0,0 +1,53 @@
+const field = document.querySelector('.window')
+
+const redSlider = document.querySelector('.R')
+const greenSlider = document.querySelector('.G')
+const blueSlider = document.querySelector('.B')
+
+const redValue = document.querySelector('.redValue')
+const greenValue = document.querySelector('.greenValue')
+const blueValue = document.querySelector('.blueValue')
+
+const sliders = document.querySelectorAll('input')
+
+
+sliders.forEach((slider)=>{
+    slider.addEventListener('input',(event)=>{
+        const {target} = event
+        const value =target.value 
+        changeColorValue(target, value)
+        const hex = calculateAndSetHexValue()
+        changeBackGroundColor(hex)
+    })
+})
+
+const changeColorValue = (target, value)=>{
+    target.nextSibling.nextSibling.innerHTML = value
+}
+
+const calculateAndSetHexValue = ()=>{
+    const [r,g,b] = [redValue.innerHTML, greenValue.innerHTML, blueValue.innerHTML]
+    const hexValue = fullColorHex(r,g,b)
+    return hexValue
+}
+
+
+const changeBackGroundColor = (hex)=>{
+    field.style.backgroundColor = `#${hex}`
+}
+
+
+const fullColorHex = function(r,g,b) {   
+    var red = rgbToHex(r);
+    var green = rgbToHex(g);
+    var blue = rgbToHex(b);
+    return red+green+blue;
+  };
+
+const rgbToHex = function (rgb) { 
+    var hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+         hex = "0" + hex;
+    }
+    return hex;
+  };
